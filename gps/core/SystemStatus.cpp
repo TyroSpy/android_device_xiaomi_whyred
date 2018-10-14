ce4b3c0f7ccfd15f41a68daecc288aca84ecd2fd
TyroSpy <tyrospy@gmail.com>
2018-10-14 19:52:09 +0300

    Update GPS sources from LA.UM.6.2.r1-11300-sdm660.0

--- gps/core/SystemStatus.cpp
+++ gps/core/SystemStatus.cpp
@@ -151,6 +151,7 @@ private:
         eAgcGlo = 20,
         eAgcBds = 21,
         eAgcGal = 22,
+        eMax0 = eAgcGal,
         eLeapSeconds = 23,
         eLeapSecUnc = 24,
         eMax
@@ -187,7 +188,7 @@ public:
         : SystemStatusNmeaBase(str_in, len_in)
     {
         memset(&mM1, 0, sizeof(mM1));
-        if (mField.size() < eMax) {
+        if (mField.size() <= eMax0) {
             LOC_LOGE("PQWM1parser - invalid size=%zu", mField.size());
             mM1.mTimeValid = 0;
             return;
@@ -214,8 +215,10 @@ public:
         mM1.mAgcGlo = atof(mField[eAgcGlo].c_str());
         mM1.mAgcBds = atof(mField[eAgcBds].c_str());
         mM1.mAgcGal = atof(mField[eAgcGal].c_str());
-        mM1.mLeapSeconds = atoi(mField[eLeapSeconds].c_str());
-        mM1.mLeapSecUnc = atoi(mField[eLeapSecUnc].c_str());
+        if (mField.size() > eLeapSecUnc) {
+            mM1.mLeapSeconds = atoi(mField[eLeapSeconds].c_str());
+            mM1.mLeapSecUnc = atoi(mField[eLeapSecUnc].c_str());
+        }
     }
 
     inline SystemStatusPQWM1& get() { return mM1;} //getparser
@@ -1668,7 +1671,7 @@ bool SystemStatus::setDefaultReport(void)
 bool SystemStatus::eventConnectionStatus(bool connected, int8_t type)
 {
     // send networkinof dataitem to systemstatus observer clients
-    SystemStatusNetworkInfo s(type, "", "", false, connected, false);
+    SystemStatusNetworkInfo s(type, "", "", connected);
     mSysStatusObsvr.notify({&s});
 
     return true;
